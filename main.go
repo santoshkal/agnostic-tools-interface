624af03d31c8831e765edf9c02915e2aa7f45b32
Santosh <ksantosh@intelops.dev>
2024-05-09 13:01:31 +0530

    WIP: Search functionality2
    Signed-off-by: Santosh <ksantosh@intelops.dev>

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"os"
 
 	"github.com/meilisearch/meilisearch-go"
 )
@@ -23,26 +20,35 @@ func main() {
 		fmt.Printf("Failed to create index: %v", err)
 	}
 
-	jsonFile, _ := os.Open("apps.json")
-	defer jsonFile.Close()
+	// jsonFile, _ := os.Open("apps.json")
+	// defer jsonFile.Close()
 
-	byteValue, _ := io.ReadAll(jsonFile)
-	var apps []map[string]interface{}
-	json.Unmarshal(byteValue, &apps)
+	// // byteValue, _ := io.ReadAll(jsonFile)
+	// var appMap map[string]interface{}
+	// err = json.NewDecoder(jsonFile).Decode(&appMap)
+	// if err != nil {
+	// 	fmt.Println("Error decoding JSON:", err)
+	// 	return
+	// }
 
-	resp, err := client.Index("apps").AddDocuments(apps)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Document apps Added: %#v\n", resp.Status)
+	// apps := []map[string]interface{}{appMap}
+
+	// resp, err := client.Index("apps").AddDocuments(apps)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Document apps Added: %#v\n", resp.Status)
+	// fmt.Printf("Document apps Added: %#v\n", resp.TaskUID)
+	// fmt.Printf("Document apps Added: %#v\n", resp.IndexUID)
+	// fmt.Printf("Document apps Added: %#v\n", resp.Type)
 
 	fa := []string{"type", "description"}
 	client.Index("apps").UpdateFilterableAttributes(&fa)
 
-	i, err := client.GetIndex("apps")
-	if err != nil {
-		fmt.Printf("Failed to get Index: %v", err)
-	}
+	// i, err := client.GetStats("apps")
+	// if err != nil {
+	// 	fmt.Printf("Failed to get Index: %v", err)
+	// }
 
 	f, err := client.Index("apps").GetFilterableAttributes()
 	if err != nil {
@@ -50,10 +56,10 @@ func main() {
 	}
 
 	s, err := client.Index("apps").Search("DeploymentList", &meilisearch.SearchRequest{
-		AttributesToRetrieve: []string{"name", "description"},
-		// 	// Filter: [][]string{
-		// 	// 	{"name = \"limit\""},
-		// 	// 	{"description = limit is a maximum number of responses to return for a list call"},
+		AttributesToRetrieve: []string{"type", "description"},
+		// Filter: [][]string{
+		// 	[]string{"name = \"limit\""},
+		// 	[]string{"description: limit is a maximum number of responses to return for a list call"},
 		// },
 	})
 	if err != nil {
@@ -63,7 +69,7 @@ func main() {
 	// if err != nil {
 	// 	fmt.Printf("Failed to get task: %v", err)
 	// }
-	fmt.Printf("Found Index: %v\n", i.UID)
+	// fmt.Printf("Found Index: %v\n", i.UID)
 	fmt.Printf("Found Search: %v\n", s.Hits)
 
 	fmt.Printf("Found Attributes: %v\n", f)
